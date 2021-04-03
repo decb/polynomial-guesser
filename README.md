845130742528c77c462416505cfb9bde08229ce1
decb <decb@users.noreply.github.com>
2021-04-03 13:15:29 +0100

    core guessing logic

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # polynomial-guesser
+
+Think of a polynomial in _x_ of arbitrary degree with non-negative integer coefficients, this program can guess it given just two points on the polynomial.
+
+For example, given _x<sup>2</sup> + 3x + 9_:
+
+```
+$ polynomial-guesser
+What is the value for x = 1? 13
+What is the value for x = 14? 247
+[9,3,1]
+```
+
+## Installing and running
+
+Requires the Haskell tool [Stack](https://docs.haskellstack.org/en/stable/README/).
+
+```bash
+stack build
+stack exec polynomial-guesser
+```
+
+## References
+
+1. Math &cap; Programming, _Learning a single-variable polynomial, or the power of adaptive queries_
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,27 @@
-module Main where
+module Main
+  ( main
+  ) where
+
+import           Numeric.Natural (Natural)
+import           System.IO       (hFlush, stdout)
+import           Text.Read       (readMaybe)
+
+import           Guess           (guess)
 
 main :: IO ()
-main = print "..."
+main = do
+  a <- promptUser 1
+  b <- promptUser (a + 1)
+  let result = guess b (a + 1)
+  putStrLn $ showPolynomial result
+
+promptUser :: Natural -> IO Natural
+promptUser x = do
+  putStr ("What is the value of your polynomial for x = " <> show x <> "? ")
+  input <- hFlush stdout >> getLine
+  case readMaybe input of
+    Just n  -> return n
+    Nothing -> promptUser x
+
+showPolynomial :: [Natural] -> String
+showPolynomial = show
